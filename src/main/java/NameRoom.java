aaa0e1448a639940422f1a516b8f5f87e025204c
Leothos_Thoren <soussou.med@live.fr>
2018-05-24 11:00:02 +0200

    Staging from 23/05 23:30:00

---
+++ src/main/java/NameRoom.java
@@ -0,0 +1,54 @@
+public enum NameRoom {
+    //Objets directement construits
+    HALL ("Le hall"),
+    WC ("La salle de bain"),
+    ROOM ("La chambre"),
+    KITCHEN ("La cuisine");
+
+    private String name;
+
+    //Constructeur
+    NameRoom(String name){
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String toString(){
+        return name;
+    }
+}
+
+
+
+
+//public enum Volume {
+//    HALL {
+//        @Override
+//        public void access() {
+//            System.out.println(roomName.getName());
+//            System.out.println("Vous allez inspecter : (Q pour quitter)");
+//            System.out.println("");
+//            System.out.println("la cuisine (K)");
+//            System.out.println("le salon (S)");
+//            System.out.println("la bibliothï¿½que (L)");
+//            choice = sc.nextLine().charAt(0);
+//
+//
+//        }
+//    }, MEDIUM {
+//        @Override
+//        public void alert() {
+//            System.out.println("Tout baigne.");
+//
+//        }
+//    }, HIGH {
+//        @Override
+//        public void alert() {
+//            System.out.println("Attention, c'est trop fort !");
+//
+//        }
+//    };
--- src/main/java/Room.java
+++ src/main/java/Room.java
@@ -1,9 +1,41 @@
 public class Room {
 
-    // Attributes : ArrayList items, ArrayList pnj
+    // Attributes : Name of room, ArrayList items, ArrayList pnj //
+
+    private static int idForIncrement = 1;
+    private int id;
+    private String name;
+    private static String item;
+
+
+    // NAME OF ROOM //
+
+    public Room(String name) {
+        this.id = idForIncrement++;
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return "Localisation : Vous êtes dans " + name;
+    }
+
+
+    // CHOICE ROOM //
+
+
+    // ITEMS OF ROOM //
+
+    public static String getItem() {
+        return item;
+    }
 
 
     // Methods : getRoom(), setRoom(), getAvailableItem(), getPnj(), getCorpse()
-
+    //
 
 }
