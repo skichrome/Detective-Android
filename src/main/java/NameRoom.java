3ab3cc01619d67de766a222320c80aa82575d89d
leothos <soussou.med@live.fr>
2018-06-20 13:29:31 +0200

    Clean NameRoom

--- src/main/java/NameRoom.java
+++ src/main/java/NameRoom.java
@@ -1,13 +1,13 @@
 public enum NameRoom {
 
-    HALL("Le hall", "la cuisine (C),le salon (S),La biblio (B)"),
-    WC("La salle de bain", "le jardin (J), le salon (S), le dortoir (D)"),
-    BED_ROOM("Le dortoir", "la salle de bain (W),le bureau (O)"),
-    LIVING_ROOM("le salon", "la cuisine (C),le hall (H),la salle de bain (W),le bureau (O),le jardin (J)"),
-    OFFICE("le bureau", "la bibliothèque (B),le salon (S),le dortoir (D)"),
-    KITCHEN("La cuisine", "le salon (S),le hall (H)"),
+    HALL("le hall", "la bibliothèque (B),la cuisine (C),le salon (S)"),
+    WC("la salle de bain", "le dortoir (D),le jardin (J),le salon (S)"),
+    BED_ROOM("le dortoir", "le bureau (O),la salle de bain (W)"),
+    LIVING_ROOM("le salon", "le bureau (O),la cuisine (C),le hall (H),le jardin (J),la salle de bain (W)"),
+    OFFICE("le bureau", "la bibliothèque (B),le dortoir (D),le salon (S)"),
+    KITCHEN("la cuisine", "le hall (H),le salon (S)"),
     GARDEN("le jardin", "la salle de bain (W),le salon (S)"),
-    LIBRARY("la bibliothèque", "le hall (H),le bureau (O)");
+    LIBRARY("la bibliothèque", "le bureau (O),le hall (H)");
 
     private String name;
     private String availableRooms;
