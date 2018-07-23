e0011ff58c317fb31ffdd31b30727dbd5e701416
lilidemon <adlibitum7@vivaldi.net>
2018-07-23 22:21:04 +0200

    add constructor and nameItems

--- src/main/java/Items.java
+++ src/main/java/Items.java
@@ -1,20 +1,36 @@
 
 public enum Items {
 
-	CORDE,
-    BRIQUET,
-    MECHE,//de cheveu
-    LIVRE,
-    LETTRE,
-    VERRE,//verre à pied
-    COUTEAU,
-    PORTABLE,//téléphone
-    MOUCHOIR,
-    STYLO,
-    CLE,//usb
-    CARTE,//de visite
-    CALCULATRICE,
-    DISQUE,//dur
-    LAMPE;//de poche
+	ROPE("corde"),
+    LIGHTER("briquet"),
+    STRAND("mèche de cheveux"),
+    BOOK("livre"),
+    LETTER("lettre"),
+    WINEGLASS("verre à pied"),
+    KNIFE("couteau"),
+    CELLULAR("téléphone portable"),
+    TISSUE("mouchoir"),
+    PEN("stylo"),
+    USBDRIVE("clé usb"),
+    BUSINESSCARD("carte de visite"),
+    CALCULATOR("calculatrice"),
+    HARDDISK("disque dur"),
+    FLASHLIGHT("lampe de poche");
+	
+	private String nameItems;
+	
+	Items(String nameItems) {
+        this.nameItems = nameItems;
+    }
+	
+	public String getName() {
+        return nameItems;
+    }
+
+    public String toString() {
+        return nameItems;
+    }
+	
+	
 	
 }
