7f37c778a5cb765ad7ca8275eebce104c9e12c9b
lilidemon <adlibitum7@vivaldi.net>
2018-10-29 23:53:22 +0100

    creation of splashscreen, by aurelia. add backgroundscreen in drawable, modification in colors and screens xml files. add function for start app in landscape in MainActivity.java. construction of RelativeLayout with ImageView and TextView

--- app/src/main/java/com/student/android/detectiveandroid/MainActivity.java
+++ app/src/main/java/com/student/android/detectiveandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.student.android.detectiveandroid;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,6 +10,9 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
