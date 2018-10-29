626dc3720af2dda485942ab7dc7874ef1b789351
skichrome <toni.campeol@outlook.fr>
2018-10-29 22:35:40 +0100

    added clean Android Studio project and Travis config file

---
+++ app/src/main/java/com/student/android/detectiveandroid/MainActivity.java
@@ -0,0 +1,15 @@
+package com.student.android.detectiveandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
