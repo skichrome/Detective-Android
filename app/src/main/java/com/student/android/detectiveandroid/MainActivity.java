ddacdf2fb93fefa4d43f2eaf5b53c24ef5580ef6
Toni <34231431+skichrome@users.noreply.github.com>
2018-11-05 23:57:40 +0100

    Added support of launching main menu activity in MainActivity.java In HomeActivity.java there is 3 buttons for a future implementation Added some drawables for images in the app and a custom button shape style Added a snapshot of color theme of the app

--- app/src/main/java/com/student/android/detectiveandroid/MainActivity.java
+++ app/src/main/java/com/student/android/detectiveandroid/MainActivity.java
@@ -1,8 +1,11 @@
 package com.student.android.detectiveandroid;
 
-import android.content.pm.ActivityInfo;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -10,7 +13,18 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.activity_main_btn)
+    void OnClickBtn() { this.startHomeActivity(); }
+
+    //todo ajouter un handler pour avoir un temps avant de passer automatiquement à l'activité suivante
+
+    private void startHomeActivity()
+    {
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
