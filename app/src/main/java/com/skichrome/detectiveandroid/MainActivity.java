6a40c2fef5267455566af9c2df786f2a3bb77622
skichrome <toni.campeol@outlook.fr>
2018-11-07 13:26:10 +0100

    [Add] Automatic launch of main menu after a timeout [Add] CanaryLeaks to detect memory leaks [Add] Landscape layout for activities

--- app/src/main/java/com/skichrome/detectiveandroid/MainActivity.java
+++ app/src/main/java/com/skichrome/detectiveandroid/MainActivity.java
@@ -4,11 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity
 {
+    private Timer wait = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -17,10 +22,39 @@ public class MainActivity extends AppCompatActivity
         ButterKnife.bind(this);
     }
 
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        startAutomatically();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        if (wait != null) wait.cancel();
+    }
+
     @OnClick(R.id.activity_main_btn)
     void OnClickBtn() { this.startHomeActivity(); }
 
-    //todo ajouter un handler pour avoir un temps avant de passer automatiquement à l'activité suivante
+    private void startAutomatically()
+    {
+        wait = new Timer();
+        wait.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                runOnUiThread(new Runnable()
+                {
+                    @Override
+                    public void run() { startHomeActivity(); }
+                });
+            }
+        },5000);
+    }
 
     private void startHomeActivity()
     {
