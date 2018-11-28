ee9b072cef6ee72ab2beaa67b8f33e1fe947997a
skichrome <toni.campeol@outlook.fr>
2018-11-28 18:31:35 +0100

    [Update] Toolbar deleted in activities, except GameActivity.java, toolbar has been updated with back button and map / pause icon. [Update] Color style [Update] Icons in the app

--- app/src/main/java/com/skichrome/detectiveandroid/activities/GameActivity.java
+++ app/src/main/java/com/skichrome/detectiveandroid/activities/GameActivity.java
@@ -1,10 +1,16 @@
 package com.skichrome.detectiveandroid.activities;
 
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.skichrome.detectiveandroid.R;
 import com.skichrome.detectiveandroid.base.BaseActivity;
 import com.skichrome.detectiveandroid.fragments.StartGameFragment;
+
+import butterknife.BindView;
 
 public class GameActivity extends BaseActivity
 {
@@ -12,7 +18,9 @@ public class GameActivity extends BaseActivity
     // Fields
     //=========================================
 
-    private StartGameFragment startGameFragment;
+    @BindView(R.id.activity_toolbar) Toolbar mToolbar;
+
+    private StartGameFragment mStartGameFragment;
 
     //=========================================
     // Superclass Methods
@@ -21,9 +29,54 @@ public class GameActivity extends BaseActivity
     @Override
     protected int getActivityLayout() { return R.layout.activity_game; }
     @Override
-    protected void configureActivity() { this.configureStartGameFragment(); }
+    protected void configureActivity()
+    {
+        this.configureToolbar();
+        this.configureStartGameFragment();
+    }
     @Override
     protected void updateActivity() { }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        // Todo save the game before quit
+    }
+
+    //=========================================
+    // Toolbar methods
+    //=========================================
+
+    private void configureToolbar()
+    {
+        setSupportActionBar(mToolbar);
+        mToolbar.inflateMenu(R.menu.activity_game_menu);
+        ActionBar ab = getSupportActionBar();
+        if (ab != null)
+        {
+            ab.setDisplayShowTitleEnabled(false);
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.activity_game_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.activity_game_menu_pause : return true;
+            case R.id.activity_game_menu_map_access : return true;
+            default : return super.onOptionsItemSelected(item);
+        }
+    }
 
     //=========================================
     // Fragment Methods
@@ -36,7 +89,7 @@ public class GameActivity extends BaseActivity
 
     private void configureStartGameFragment()
     {
-        if (startGameFragment == null) startGameFragment = StartGameFragment.newInstance();
-        displayFragment(startGameFragment);
+        if (mStartGameFragment == null) mStartGameFragment = StartGameFragment.newInstance();
+        displayFragment(mStartGameFragment);
     }
 }
